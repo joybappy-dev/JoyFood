29753fda68ac49db31dad18d258694f1c0cd16a2
joybappy2 <fan328547@gmail.com>
2026-03-09 08:35:29 +0600

    make logo responsive

--- src/app/components/Logo/page.jsx
+++ src/app/components/Logo/page.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
     return (
         <figure>
             {/* <img className='w-28 ' src="/logo.png" alt="logo image" /> */}
-            <h2 className='text-3xl md:text-4xl font-semibold py-4 text-yellow-400'>Joy Food</h2>
+            <h2 className='text-2xl md:text-4xl font-semibold py-4 text-yellow-400'>Joy Food</h2>
         </figure>
     );
 };
