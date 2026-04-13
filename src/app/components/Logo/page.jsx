cdc8832e8933281138845375690a3e42620b6589
Joy Bappy <joybappy2025@gmail.com>
2026-04-13 09:02:32 +0600

    add logo image and change favicon

--- src/app/components/Logo/page.jsx
+++ src/app/components/Logo/page.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Logo = () => {
-    return (
-        <figure>
-            {/* <img className='w-28 ' src="/logo.png" alt="logo image" /> */}
-            <h2 className='text-2xl md:text-4xl font-semibold py-4 text-yellow-400'>Joy Food</h2>
-        </figure>
-    );
+  return (
+    <figure>
+      <img className="w-20" src="/logo.png" alt="logo" />
+    </figure>
+  );
 };
 
 export default Logo;
